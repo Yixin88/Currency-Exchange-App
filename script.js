52d2b213065c9fb6ccffa1a509f879dd204affb4
Yixin88 <yixin88@hotmail.co.uk>
2022-10-25 23:34:19 +0100

    added dynamic addess depending on user inputs

--- script.js
+++ script.js
@@ -2,14 +2,25 @@
 var myHeaders = new Headers();
 myHeaders.append("apikey", "keiEm6mToK1Lqp2nh7HCSTnPAEc5OBmu");
 
+const getAddress = () => {
+  const from = document.getElementById("from").value;
+  const to = document.getElementById("to").value;
+  const amount = document.getElementById("amount").value;
+  return `https://api.apilayer.com/exchangerates_data/convert?to=${to}&from=${from}&amount=${amount}`;
+}
 
-var requestOptions = {
+const convertBtn = document.getElementById("convertBtn");
+
+convertBtn.addEventListener('click', () => {
+  var requestOptions = {
     method: 'GET',
     redirect: 'follow',
     headers: myHeaders
   };
   
-fetch("https://api.apilayer.com/exchangerates_data/convert?to=gbp&from=eur&amount=10", requestOptions)
-.then(response => response.text())
-.then(result => console.log(result))
-.catch(error => console.log('error', error));
+  fetch(getAddress(), requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
+});
+  
