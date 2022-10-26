ab9cbd2f7e51c04b00578cdfa7b4173be60ac1ab
Yixin88 <yixin88@hotmail.co.uk>
2022-10-26 22:49:18 +0100

    added template literal output, reset btn resets all field, error msg

--- script.js
+++ script.js
@@ -9,7 +9,17 @@ const getAddress = () => {
   return `https://api.apilayer.com/exchangerates_data/convert?to=${to}&from=${from}&amount=${amount}`;
 }
 
+const resetInput = () => {
+  document.getElementById("from").value = "";
+  document.getElementById("to").value = "";
+  document.getElementById("amount").value = "";
+}
+
 const convertBtn = document.getElementById("convertBtn");
+const resetBtn = document.getElementById("resetBtn");
+resetBtn.addEventListener('click', () => {
+  resetInput();
+})
 
 convertBtn.addEventListener('click', () => {
   var requestOptions = {
@@ -21,8 +31,19 @@ convertBtn.addEventListener('click', () => {
   fetch(getAddress(), requestOptions)
   .then(response => response.json())
   .then(data => {
-    const result = data.result;
-    document.getElementById("outputArea").innerText = result;
+    const result = parseFloat(data.result);
+    const from = data.query.from.toString();
+    const to = data.query.to.toString();
+    const amount = data.query.amount.toString();
+    console.log(data)
+    if (document.getElementById("amount").value.trim() === "" || document.getElementById("from").value.trim() === "" || document.getElementById("to").value.trim() === "") {
+      document.getElementById("outputArea").innerText = `Please fill in all input`;
+    } else if (result !== undefined) {
+      document.getElementById("outputArea").innerText = `${amount} ${from} = ${result.toFixed(3)} ${to}`;
+    } else {
+      document.getElementById("outputArea").innerText = `You have inputed an invalid currency!`;
+    }
+      
   })
   .catch(error => console.log('error', error));
 
