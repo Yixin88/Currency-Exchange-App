8f519782b4dc91537f936030f3eb819e3feef7d3
Yixin88 <yixin88@hotmail.co.uk>
2022-11-10 00:31:29 +0000

    fixed issue where if theres a space after a currency, it returns an error

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ var myHeaders = new Headers();
 myHeaders.append("apikey", "keiEm6mToK1Lqp2nh7HCSTnPAEc5OBmu");
 
 const getAddress = () => {
-  const from = document.getElementById("from").value;
-  const to = document.getElementById("to").value;
-  const amount = document.getElementById("amount").value;
+  const from = document.getElementById("from").value.trim();
+  const to = document.getElementById("to").value.trim();
+  const amount = document.getElementById("amount").value.trim();
   return `https://api.apilayer.com/exchangerates_data/convert?to=${to}&from=${from}&amount=${amount}`;
 }
 
